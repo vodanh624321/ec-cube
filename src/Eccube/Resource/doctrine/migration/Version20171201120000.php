8dd8bf508789dd916488c3789410fef28c768178
lqdung <lqdung@lockon-vn.com>
2017-12-01 19:12:58 +0700

    Add data

--- src/Eccube/Resource/doctrine/migration/Version20171201120000.php
+++ src/Eccube/Resource/doctrine/migration/Version20171201120000.php
@@ -15,6 +15,10 @@ class Version20171201120000 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
+//        INSERT INTO `dtb_block` (`block_id`, `device_type_id`, `block_name`, `file_name`, `create_date`, `update_date`, `logic_flg`, `deletable_flg`) VALUES (11, 10, 'Search smart', 'search_smart', '2017-12-01 20:17:29', '2017-12-01 20:17:29', 0, 1);
+//        INSERT INTO `dtb_block` (`block_id`, `device_type_id`, `block_name`, `file_name`, `create_date`, `update_date`, `logic_flg`, `deletable_flg`) VALUES (12, 10, 'breadcrumb', 'breadcrumb', '2017-12-01 20:22:07', '2017-12-01 20:22:07', 0, 1);
+//        INSERT INTO `dtb_block_position` (`page_id`, `target_id`, `block_id`, `block_row`, `anywhere`) VALUES (1, 3, 12, 1, 1);
+//        INSERT INTO `dtb_block_position` (`page_id`, `target_id`, `block_id`, `block_row`, `anywhere`) VALUES (1, 4, 11, 1, 1);
 //        $app = \Eccube\Application::getInstance();
 //        /** @var EntityManager $em */
 //        $em = $app["orm.em"];
